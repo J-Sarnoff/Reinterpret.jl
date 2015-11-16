21f2c4e8c42a47c9fe969ebd8c602cd39d8ca35f
J-Sarnoff <jeffreysarnoff@gmail.com>
2015-11-15 20:02:57 -0500

    Update Reinterprets.jl

--- src/Reinterprets.jl
+++ src/Reinterprets.jl
@@ -1,29 +1,26 @@
 module Reinterprets
 
-import Base: unbox, box
-export reinterprets
-
-@inline reinterprets(x,y) = reinterpret(x,y)
+import Base: unbox, box, reinterpret
 
 for (utype,itype,ftype) in ((:UInt16, :Int16, :Float16), (:UInt32, :Int32, :Float32), (:UInt64, :Int64, :Float64))
     @eval begin
-        reinterprets(::Type{Signed}, x::($utype))   = box($itype,unbox($utype,x))
-        reinterprets(::Type{Signed}, x::($itype))   = x
-        reinterprets(::Type{Signed}, x::($ftype))   = box($itype,unbox($ftype,x))
-        reinterprets(::Type{Unsigned}, x::($utype)) = x
-        reinterprets(::Type{Unsigned}, x::($itype)) = box($utype,unbox($itype,x))
-        reinterprets(::Type{Unsigned}, x::($ftype)) = box($utype,unbox($ftype,x))
-        reinterprets(::Type{AbstractFloat}, x::($utype)) = box($ftype,unbox($utype,x))
-        reinterprets(::Type{AbstractFloat}, x::($itype)) = box($ftype,unbox($itype,x))
-        reinterprets(::Type{AbstractFloat}, x::($ftype)) = x
+        reinterpret(::Type{Signed}, x::($utype))   = box($itype,unbox($utype,x))
+        reinterpret(::Type{Signed}, x::($itype))   = x
+        reinterpret(::Type{Signed}, x::($ftype))   = box($itype,unbox($ftype,x))
+        reinterpret(::Type{Unsigned}, x::($utype)) = x
+        reinterpret(::Type{Unsigned}, x::($itype)) = box($utype,unbox($itype,x))
+        reinterpret(::Type{Unsigned}, x::($ftype)) = box($utype,unbox($ftype,x))
+        reinterpret(::Type{AbstractFloat}, x::($utype)) = box($ftype,unbox($utype,x))
+        reinterpret(::Type{AbstractFloat}, x::($itype)) = box($ftype,unbox($itype,x))
+        reinterpret(::Type{AbstractFloat}, x::($ftype)) = x
     end
 end
 for (utype,itype) in ((:UInt8, :Int8), (:UInt128, :Int128))
     @eval begin
-        reinterprets(::Type{Signed}, x::($utype))   = box($itype,unbox($utype,x))
-        reinterprets(::Type{Signed}, x::($itype))   = x
-        reinterprets(::Type{Unsigned}, x::($utype)) = x
-        reinterprets(::Type{Unsigned}, x::($itype)) = box($utype,unbox($itype,x))
+        reinterpret(::Type{Signed}, x::($utype))   = box($itype,unbox($utype,x))
+        reinterpret(::Type{Signed}, x::($itype))   = x
+        reinterpret(::Type{Unsigned}, x::($utype)) = x
+        reinterpret(::Type{Unsigned}, x::($itype)) = box($utype,unbox($itype,x))
     end
 end
 
